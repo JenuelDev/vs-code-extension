2d80e12bc9086d6581cb32c8bcd0a89abe21665a
Jenuel Oras Ganawed <me@jenuel.dev>
2026-03-14 22:02:02 +0800

    updates

--- src/test/fixtures/laravel-react/routes/v2/sender-names/sender_names.php
+++
@@ -1,6 +0,0 @@
-<?php
-
-use Illuminate\Support\Facades\Route;
-
-Route::get('/', 'SenderNameController@index')->name('api.senderNames.index');
-Route::get('/{senderName}', 'SenderNameController@show')->name('api.senderNames.show');
--- src/test/fixtures/laravel-react/routes/web/api/admin/users.php
+++
@@ -1,6 +0,0 @@
-<?php
-
-use Illuminate\Support\Facades\Route;
-
-Route::get('/users', fn () => ['source' => 'web-api-admin'])->name('web.api.admin.users.index');
-Route::get('/users/{user}', fn (string $user) => ['user' => $user])->name('web.api.admin.users.show');
