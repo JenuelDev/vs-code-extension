fd4c45fd6fa51b16f4780bd0a06fa0818f105ba6
Jenuel Oras Ganawed <me@jenuel.dev>
2026-03-14 22:02:44 +0800

    Reapply "updates"
    This reverts commit 67552db4a0a40c008654a9b476769c3ba81fe8c5.

--- src/test/fixtures/laravel-react/routes/api/users.php
+++
@@ -1,7 +0,0 @@
-<?php
-
-use Illuminate\Support\Facades\Route;
-
-Route::get('/users', 'UserController@index')->name('api.users.index');
-Route::post('/users', 'UserController@store')->name('api.users.store');
-Route::get('/users/{user}', 'UserController@show')->name('api.users.show');
--- src/test/fixtures/laravel-react/routes/web/api/reports.php
+++
@@ -1,6 +0,0 @@
-<?php
-
-use Illuminate\Support\Facades\Route;
-
-Route::get('/reports', fn () => ['source' => 'web-api'])->name('web.api.reports.index');
-Route::get('/reports/{report}', fn (string $report) => ['report' => $report])->name('web.api.reports.show');
